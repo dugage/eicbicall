da612128763afca48f3bde1d559080fe701781b6
dugage <info@abintesa.es>
2018-12-30 22:12:11 +0100

    cambios config

--- application/config/database.php
+++ application/config/database.php
@@ -97,7 +97,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '104.248.245.217',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '&4222SFCrb',
 	'database' => 'eicbi_callcenter',
